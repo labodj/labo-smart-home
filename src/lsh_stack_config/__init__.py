f6511e61413c8fd2279b64e01e4c8a527b204124
Jacopo Labardi <2527836+labodj@users.noreply.github.com>
2026-08-18 02:13:46 +0200

    chore: release v0.3.0

--- src/lsh_stack_config/__init__.py
+++ src/lsh_stack_config/__init__.py
@@ -3,6 +3,6 @@
 from .composer import compose_stack
 from .parser import load_stack_config
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 __all__ = ["__version__", "compose_stack", "load_stack_config"]
